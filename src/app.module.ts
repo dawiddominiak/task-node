d93694fd0e1c41dda0ca459ee1fd7d887e2afb12
dawiddominiak <poczta@dawiddominiak.pl>
2020-01-21 22:11:56 +0100

    Get rid of not needed app controller and app service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { CartModule } from './cart/cart.module';
-import { CurrencyModule } from './currency/currency.module';
 import { CommonModule } from './common/common.module';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
   imports: [CartModule, CurrencyModule, CommonModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
